7ae0c1570d9766efd79fe7e0547809dae3d7fbda
张阳 <sdzy890819@126.com>
2019-02-26 11:51:53 +0800

    添加 省市区

--- orderservice/src/main/java/com.fdz.order/dto/ReceivingAddressDto.java
+++ orderservice/src/main/java/com.fdz.order/dto/ReceivingAddressDto.java
@@ -13,15 +13,18 @@ public class ReceivingAddressDto {
     @NotNull(message = "收货人不可以为空")
     private String name;
 
-    /**
-     * 地址
-     */
-    @NotNull(message = "收货地址不可以为空")
+    @NotNull(message = "省份不可以为空, 省")
+    private String province;
+
+    @NotNull(message = "城市不可以为空，市")
+    private String city;
+
+    @NotNull(message = "区域不可以为空, 区")
+    private String area;
+
+    @NotNull(message = "详细收货地址不可以为空")
     private String address;
 
-    /**
-     * 手机号
-     */
     @NotNull(message = "收货人手机号不可以为空")
     private String mobile;
 }
