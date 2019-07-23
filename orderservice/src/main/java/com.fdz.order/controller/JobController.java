b5c99220a444c1b395055607360a5e6bdc404a26
张阳 <sdzy890819@126.com>
2019-07-23 21:09:35 +0800

    修改

--- orderservice/src/main/java/com.fdz.order/controller/JobController.java
+++ orderservice/src/main/java/com.fdz.order/controller/JobController.java
@@ -6,7 +6,6 @@ import com.fdz.order.service.OrderService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
